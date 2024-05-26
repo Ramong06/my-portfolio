3c3e8ef4870d39e70e6be82c210be69a6f2352a8
Ramon Garza <ramong5406@gmail.com>
2024-05-25 21:55:02 -0500

    fixed link to github

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="container mx-auto flex flex-col items-center justify-between md:flex-row px-4">
         <p className="mb-4 md:mb-0">Created by Ramon Garza and the power of Next.js</p>
         <div className="flex space-x-4">
-          <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer" className="text-white hover:text-lime-500">
+          <a href="https://github.com/Ramong06" target="_blank" rel="noopener noreferrer" className="text-white hover:text-lime-500">
           <i className="devicon-github-original-wordmark text-6xl mx-12"></i>
           </a>
           <a href="https://www.linkedin.com/in/ramon-garza-software-engineer/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-lime-500">
